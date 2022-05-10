37352cec03d5dde2e225384617101527252a961c
Elenche <elena.naumoska96@gmail.com>
2022-05-10 13:13:22 +0200

    feat(android): add config to configure showing location alert on startup

--- src/android/LocationManager.java
+++ src/android/LocationManager.java
@@ -82,6 +82,8 @@ public class LocationManager extends CordovaPlugin implements BeaconConsumer {
     private static final String SAMPLE_EXPIRATION_MILLISECOND = "com.unarin.cordova.beacon.android.altbeacon.SampleExpirationMilliseconds";
     private static final int DEFAULT_SAMPLE_EXPIRATION_MILLISECOND = 20000;
     private static final int DEFAULT_FOREGROUND_SCAN_PERIOD = 1100;
+    private static final String ENABLE_LOCATION_PERMISSION_ALERT = "com.unarin.cordova.beacon.android.altbeacon.EnableLocationPermissionAlert";
+    private static final boolean DEFAULT_ENABLE_LOCATION_PERMISSION_ALERT = true;
     private static int CDV_LOCATION_MANAGER_DOM_DELEGATE_TIMEOUT = 30;
     private static final int BUILD_VERSION_CODES_M = 23;
 
@@ -299,31 +301,36 @@ public class LocationManager extends CordovaPlugin implements BeaconConsumer {
                 return;
             }
 
-            final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            builder.setTitle("This app needs location access");
-            builder.setMessage("Please grant location access so this app can detect beacons.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @SuppressLint("NewApi")
-                @Override
-                public void onDismiss(final DialogInterface dialog) {
+            final boolean enableLocationPermissionAlert = this.preferences.getBoolean(
+                ENABLE_LOCATION_PERMISSION_ALERT, DEFAULT_ENABLE_LOCATION_PERMISSION_ALERT);
 
-                    try {
-                        requestPermissionsMethod.invoke(activity,
-                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                                PERMISSION_REQUEST_COARSE_LOCATION
-                        );
-                    } catch (IllegalAccessException e) {
-                        Log.e(TAG, "IllegalAccessException while requesting permission for " +
-                                "ACCESS_COARSE_LOCATION:", e);
-                    } catch (InvocationTargetException e) {
-                        Log.e(TAG, "InvocationTargetException while requesting permission for " +
-                                "ACCESS_COARSE_LOCATION:", e);
-                    }
-                }
-            });
+            if (enableLocationPermissionAlert) {
+              final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+              builder.setTitle("This app needs location access");
+              builder.setMessage("Please grant location access so this app can detect beacons.");
+              builder.setPositiveButton(android.R.string.ok, null);
+              builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                  @SuppressLint("NewApi")
+                  @Override
+                  public void onDismiss(final DialogInterface dialog) {
 
-            builder.show();
+                      try {
+                          requestPermissionsMethod.invoke(activity,
+                                  new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                  PERMISSION_REQUEST_COARSE_LOCATION
+                          );
+                      } catch (IllegalAccessException e) {
+                          Log.e(TAG, "IllegalAccessException while requesting permission for " +
+                                  "ACCESS_COARSE_LOCATION:", e);
+                      } catch (InvocationTargetException e) {
+                          Log.e(TAG, "InvocationTargetException while requesting permission for " +
+                                  "ACCESS_COARSE_LOCATION:", e);
+                      }
+                  }
+              });
+
+              builder.show();
+            }
 
         } catch (final IllegalAccessException e) {
             Log.w(TAG, "IllegalAccessException while checking for ACCESS_COARSE_LOCATION:", e);
